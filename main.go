294ebc18c8bf5772e2fde77fd2176d4020ed1bad
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-04-17 09:26:36 +0000

    fix

--- main.go
+++ main.go
@@ -1,94 +1,15 @@
-package main
-
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START cloud_sql_postgres_databasesql_connect_tcp]
-// [START cloud_sql_postgres_databasesql_connect_tcp_sslcerts]
-// [START cloud_sql_postgres_databasesql_sslcerts]
+package cloudsql
 
 import (
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	// Note: If connecting using the App Engine Flex Go runtime, use
 	// "github.com/jackc/pgx/stdlib" instead, since v4 requires
 	// Go modules which are not supported by App Engine Flex.
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	db, err := connectToCloudSQL()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "Failed to connect to Cloud SQL")
-		return
-	}
-	defer db.Close()
-	fmt.Fprint(w, "Connected to Cloud SQL successfully!")
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "Cloud SQL "
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
-}
-
-func connectToCloudSQL() (*sql.DB, error) {
-	mustGetenv := func(k string) string {
-		v := os.Getenv(k)
-		if v == "" {
-			log.Fatalf("Fatal Error in connect_tcp.go: %s environment variable not set.", k)
-		}
-		return v
-	}
-	var (
-		dbUser     = mustGetenv("DB_USER")       // e.g. 'my-db-user'
-		dbPassword = mustGetenv("DB_PASS")       // e.g. 'my-db-password'
-		dbHost     = mustGetenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
-		dbPort     = mustGetenv("DB_PORT")       // e.g. '5432'
-		dbName     = mustGetenv("DB_NAME")       // e.g. 'my-database'
-	)
-	dbURI := fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s",
-		dbHost, dbUser, dbPassword, dbPort, dbName)
-
-	dbPool, err := sql.Open("pgx", dbURI)
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %v", err)
-	}
-	return dbPool, nil
-}
-
-/*
 // connectTCPSocket initializes a TCP connection pool for a Cloud SQL
 // instance of Postgres.
 func connectTCPSocket() (*sql.DB, error) {
@@ -124,4 +45,3 @@ func connectTCPSocket() (*sql.DB, error) {
 
 	return dbPool, nil
 }
-*/
